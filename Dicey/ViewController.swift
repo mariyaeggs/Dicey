b898dea2d8667f382c7e95881656ba6667011271
mariyaeggs <mariyawilson@live.com>
2018-01-29 09:41:06 -0800

    Randomize die when UIButton tapped.

--- Dicey/ViewController.swift
+++ Dicey/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
    
+   /* Variables */
+   var randomDiceIndex1 : Int = 0
+   var randomDiceIndex2 : Int = 0
+   
    /* Outlets */
    @IBOutlet weak var diceImageView1: UIImageView!
    @IBOutlet weak var diceImageView2: UIImageView!
@@ -26,6 +30,11 @@ class ViewController: UIViewController {
    }
    
    @IBAction func rollButtonTapped(_ sender: UIButton) {
+      
+      randomDiceIndex1 = Int(arc4random_uniform(6))
+      randomDiceIndex2 = Int(arc4random_uniform(6))
+      
+      print("isRandom Dice1:", randomDiceIndex1, " isRandom Dice2:", randomDiceIndex2)
    }
    
 }
