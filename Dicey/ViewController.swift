71606ed0f83c912614dbb29e87eddf2272474f8b
mariyaeggs <mariyawilson@live.com>
2018-01-29 07:34:33 -0800

    Wire UIImages and UIButton in ViewController.swift.

--- Dicey/ViewController.swift
+++ Dicey/ViewController.swift
@@ -9,18 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+   /* Outlets */
+   @IBOutlet weak var diceImageView1: UIImageView!
+   @IBOutlet weak var diceImageView2: UIImageView!
+   
    override func viewDidLoad() {
       super.viewDidLoad()
       // Do any additional setup after loading the view, typically from a nib.
       
    }
-
+   
    override func didReceiveMemoryWarning() {
       super.didReceiveMemoryWarning()
       // Dispose of any resources that can be recreated.
    }
-
-
+   
+   @IBAction func rollButtonTapped(_ sender: UIButton) {
+   }
+   
 }
 
